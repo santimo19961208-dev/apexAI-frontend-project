9391fffada057d67db701d6cf704e9dce5ee27a0
Santimo <santimo19961208@gmail.com>
2026-02-27 02:30:10 -0600

    Initial ChartCore

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,9 @@
 <script lang="ts">
+	import ChartCore from '$lib/chart/ChartCore.svelte';
 </script>
 
-<svelte:head>
-	<title>apexAi</title>
-	<meta name="description" content="Svelte demo app" />
-</svelte:head>
+<div class="p-10">
+	<h1 class="mb-6 text-2xl font-bold">AI Trading Chart</h1>
 
-<section>
-	<h1>
-		Hello
-	</h1>
-</section>
+	<ChartCore />
+</div>
