e540ba47dd3830441192b75919c52219630d6cc5
Santimo <santimo19961208@gmail.com>
2026-02-27 01:37:23 -0600

    Initialize Project

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+</script>
+
+<svelte:head>
+	<title>apexAi</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	<h1>
+		Hello
+	</h1>
+</section>
